7d51abbb49372bdcb0f5a0b5ff623e42f50e656a
Luke Meyer <lmeyer@redhat.com>
2015-08-14 07:39:02 -0400

    modify how parameters are specified
    Rename some params, give them env var defaults,
    and use some as file names instead of direct values.
    
    Introduce some decent proxy TLS options to start with in the example.

--- dev.sh
+++ dev.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# This script does some setup for development use:
+# sh dev.sh --master-url=...
+
+if [[ ! -d ./secret ]]; then
+	cp -r ./secret.example ./secret
+fi
+# generally need the master CA too
+if [[ ! -e ./secret/master-ca ]]; then
+	echo "copy your master CA file to ./secret/master-ca to use this script"
+	exit 1
+fi
+
+# nodemon probably won't be in the proxy container but you'll
+# want to install it for easy development work.
 nodemon openshift-auth-proxy.js \
-  --tls-cert test-cert.pem \
-  --tls-key test-key.pem \
-  --openshift-master https://localhost:8443 \
-  --openshift-public-master https://localhost:8443 \
-  --openshift-ca /var/lib/openshift/openshift.local.config/master/ca.crt \
-  --client-id integration-services \
-  --client-secret 12345 \
+  --use-backend-host-header \
+  --debug \
+  --client-id integration-proxy \
   $@
+
+# some args you'll need to fill in yourself:
+# --backend http://localhost:8080/ \
+# --master-url https://internal.example.com:8443 \
+# --public-master-url https://external.example.com:8443 \
--- openshift-auth-proxy.js
+++ openshift-auth-proxy.js
@@ -17,89 +17,157 @@ var argv = require('yargs')
   .usage('Usage: $0 [options]')
   .wrap(120)
   .options({
-    target: {
-      describe: 'Target to proxy to',
-      demand: true
-    }, 'use-target-host-header': {
-      describe: 'Change the host header to the target URL',
-      demand: true,
+    backend: {
+      describe: 'Backend to proxy requests to once authenticated',
+      default: process.env.OAP_BACKEND_URL
+    }, 'use-backend-host-header': {
+      describe: 'Change the host header to the backend URL',
       type: 'boolean',
       default: false
-    }, 'target-ca': {
-      describe: 'CA used to valid target server'
     }, 'listen-port': {
       describe: 'Port to listen on',
-      demand: true,
-      default: 3000
+      default: Number(process.env.OAP_PROXY_PORT || 3000)
     }, 'auth-mode': {
-      describe: 'Auth mode',
-      choices:  ['oauth2', 'bearer'],
-      default: 'oauth2'
+      describe: 'Proxy auth mode',
+      choices:  ['oauth2', 'bearer', 'mutual_tls', 'dummy'],
+      default: process.env.OAP_AUTH_MODE || 'oauth2'
     }, 'user-header': {
-      describe: 'Header to set the user name on the proxied request',
-      demand: true,
-      default: 'REMOTE_USER'
+      describe: 'Header for sending user name on the proxied request',
+      default: process.env.OAP_REMOTE_USER_HEADER || 'X-Proxy-Remote-User'
     }, 'session-secret': {
-      describe: 'Secret for encrypted session cookies',
-      demand: true,
-      default: 'generated'
+      describe: 'File containing secret for encrypted session cookies',
+      default: process.env.OAP_SESSION_SECRET_FILE || 'secret/session-secret'
     }, 'session-duration': {
       describe: 'Duration for encrypted session cookies',
-      demand: true,
-      default: parseDuration('1h')
+      default: parseDuration(process.env.OAP_SESSION_DURATION || '1h')
     }, 'session-active-duration': {
       describe: 'Active duration for encrypted session cookies',
-      demand: true,
-      default: parseDuration('5m')
+      default: parseDuration(process.env.OAP_SESSION_ACTIVE_DURATION || '5m')
     }, 'session-ephemeral': {
       type: 'boolean',
       describe: 'Delete cookies on browser close',
-      demand: true,
-      default: false
+      default: true
     }, 'callback-url': {
       describe: 'oAuth callback URL',
-      demand: true,
-      default: '/auth/openshift/callback'
+      default: process.env.OAP_CALLBACK_URL || '/auth/openshift/callback'
     }, 'client-id': {
       describe: 'OAuth client ID',
-      demand: true
+      default: process.env.OAP_CLIENT_ID
     }, 'client-secret': {
-      describe: 'OAuth client secret',
-      demand: true
-    }, 'openshift-master': {
-      describe: 'OpenShift master to authenticate against',
-      demand: true,
-      default: 'https://kubernetes.default.svc'
-    }, 'openshift-public-master': {
-      describe: 'Public master address to redirect to',
-      demand: true
-    }, 'openshift-ca': {
-      describe: 'CA certificate[s] to use',
-      demand: true
-    }, 'tls-cert': {
+      describe: 'File containing OAuth client secret',
+      default: process.env.OAP_CLIENT_SECRET_FILE || 'secret/client-secret'
+    }, 'master-url': {
+      describe: 'Internal master address proxy will authenticate against',
+      default: process.env.OAP_MASTER_URL || 'https://kubernetes.default.svc.cluster.local:8443'
+    }, 'public-master-url': {
+      describe: 'Public master address for redirecting clients to',
+      default: process.env.OAP_PUBLIC_MASTER_URL
+    }, 'master-ca': {
+      describe: 'CA certificate(s) file to validate connection to the master',
+      default: process.env.OAP_MASTER_CA_FILE || 'secret/master-ca'
+    }, 'proxy-cert': {
       describe: 'Certificate file to use to listen for TLS',
-      demand: true
-    }, 'tls-key': {
+      default: process.env.OAP_PROXY_CERT_FILE || 'secret/proxy-cert'
+    }, 'proxy-key': {
       describe: 'Key file to use to listen for TLS',
-      demand: true
+      default: process.env.OAP_PROXY_KEY_FILE || 'secret/proxy-key'
+    }, 'proxy-tlsopts-file': {
+      describe: 'File containing JSON for proxy TLS options',
+      default: process.env.OAP_PROXY_TLS_FILE || 'secret/proxy-tls.json'
+    }, 'mutual-tls-ca': {
+      describe: 'CA cert file to use for validating TLS client certs under "mutual_tls" auth method',
+      default: process.env.OAP_PROXY_CA_FILE || 'secret/proxy-ca'
+    }, debug: {
+      describe: 'Show extra debug information at startup and during operations',
+      type: 'boolean',
+      default: process.env.OAP_DEBUG
     }
   })
+  .check(function(args) {
+    // yargs#demand doesn't work if we also provide a default, so check for missing/invalid args here
+    var errors = [];
+    ["backend", "proxy-cert", "proxy-key", "proxy-tlsopts-file", "session-secret",
+     "client-id", "client-secret", "master-url", "public-master-url", "master-ca",
+     "mutual-tls-ca", "callback-url"].forEach( function(val) {
+      if(args[val] == null || args[val].length == 0) {
+        errors.push("No value specified for parameter " + val);
+      }
+    });
+    if (isNaN(parseFloat(args['listen-port'])) || args['listen-port'] < 1) errors.push("Invalid listen-port specified");
+    // Now report errors if present.
+    if (errors.length > 0) throw('ERROR IN PARAMETERS:\n' + errors.join('\n'));
+    return true
+  })
   .help('help')
-  .epilog('copyright 2015')
+  .epilog('All of these parameters can be set via corresponding environment variables.')
   .argv;
 
-if (argv['session-secret'] === 'generated') {
-  require('base64url')(require('crypto').randomBytes(256)).substring(0, 256);
+// ---------------------- config --------------------------
+
+//
+// read in all the files with secrets, keys, certs
+//
+var sessionSecret;
+try {
+  sessionSecret = fs.readFileSync(argv['session-secret'], "utf8");
+} catch(err) {
+  console.error("error reading session secret: %s", JSON.stringify(e));
+} finally { // just ignore if the file is not there
+  if (sessionSecret == null ) {
+    console.error("generating session secret (will not work with scaled service)");
+    sessionSecret = require('base64url')(require('crypto').randomBytes(256)).substring(0, 256);
+  }
 };
+var clientSecret = fs.readFileSync(argv['client-secret'], "utf8").replace(/(\n|\r)/gm,"");
+var masterCA = fs.readFileSync(argv['master-ca'], "utf8");
+var mutualTlsCa;
+try { // it's optional...
+  mutualTlsCa = fs.readFileSync(argv['mutual-tls-ca'], "utf8");
+} catch(err) {
+  if (argv['auth-mode'] === 'mutual_tls') {
+    throw "No CA read for mutual TLS. Looked in: " + argv['mutual-tls-ca'];
+  } // otherwise, we don't need it
+};
+var proxyTLS = {};
+try { // also optional TLS overrides (ciphersuite etc)
+  proxyTLS = fs.readFileSync(argv['proxy-tlsopts-file'], "utf8");
+  if (argv.debug) console.log("Read TLS opts from %s: %s", argv['proxy-tlsopts-file'], proxyTLS);
+  proxyTLS = eval(proxyTLS);
+  if (proxyTLS == null || ! typeof proxyTLS === 'object') {
+    throw("TLS opts file did not evaluate to an object");
+  }
+} catch(e) {
+  console.error("Could not read TLS opts from %s; error was: %s", argv['proxy-tlsopts-file'], e);
+  proxyTLS = {};
+} finally {
+  proxyTLS['key'] = fs.readFileSync(argv['proxy-key'], "utf8");
+  proxyTLS['cert'] = fs.readFileSync(argv['proxy-cert'], "utf8");
+  if (argv.debug) {
+    console.log("in finally, proxyTLS is:");
+    console.log(proxyTLS);
+  }
+};
+if(argv['debug']) {
+  console.log("config values passed in:");
+  var arg;
+  for (arg in argv) {
+    console.log("%s", arg + ": " + argv[arg]);
+  }
+  ["sessionSecret", "clientSecret", "masterCA", "mutualTlsCa", "proxyTLS"].forEach( function(val) {
+    console.log("%s: ", val, eval(val));
+  })
+}
 
 var cas = https.globalAgent.options.ca || [];
-cas.push(fs.readFileSync(argv['openshift-ca']));
+cas.push(masterCA);
 https.globalAgent.options.ca = cas;
 
-var openshiftUserUrl = urljoin(argv['openshift-master'], '/oapi/v1/users/~');
+var openshiftUserUrl = urljoin(argv['master-url'], '/oapi/v1/users/~');
 
 var validateBearerToken = function(accessToken, refreshToken, profile, done) {
+  if (argv.debug) console.log("in validateBearerToken: ", accessToken, refreshToken, profile);
   if (!accessToken) {
+    if (argv.debug) console.log("no access token, done.");
     done();
   }
   var authOptions = {
@@ -128,10 +196,10 @@ var validateBearerToken = function(accessToken, refreshToken, profile, done) {
 switch(argv['auth-mode']) {
   case 'oauth2':
     passport.use(new OAuth2Strategy({
-        authorizationURL: urljoin(argv['openshift-public-master'], '/oauth/authorize'),
-        tokenURL: urljoin(argv['openshift-master'], '/oauth/token'),
+        authorizationURL: urljoin(argv['public-master-url'], '/oauth/authorize'),
+        tokenURL: urljoin(argv['master-url'], '/oauth/token'),
         clientID: argv['client-id'],
-        clientSecret: argv['client-secret'],
+        clientSecret: clientSecret,
         callbackURL: argv['callback-url']
       },
       validateBearerToken
@@ -154,8 +222,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 var proxy = new httpProxy.createProxyServer({
-  target: argv.target,
-  changeOrigin: argv['use-target-host-header']
+  target: argv.backend,
+  changeOrigin: argv['use-backend-host-header']
 });
 
 proxy.on('error', function(e) {
@@ -172,7 +240,7 @@ if (useSession) {
   app.use(sessions({
     cookieName: 'openshift-auth-proxy-session',
     requestKey: 'session',
-    secret: argv['session-secret'], // should be a large unguessable string
+    secret: sessionSecret, // should be a large unguessable string
     duration: parseDuration('' + argv['session-duration']), // how long the session will stay valid in ms
     activeDuration: parseDuration('' + argv['session-active-duration']), // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds,
     cookie: {
@@ -196,8 +264,10 @@ if (useSession) {
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
+    if (argv.debug) { console.log("authenticated, moving on.")}
     return next();
   }
+  if (argv.debug) { console.log("not authenticated.")}
   if (useSession) {
     req.session.returnTo = req.path;
   }
@@ -212,8 +282,5 @@ app.all('*', ensureAuthenticated, function(req, res) {
   proxy.web(req, res);
 });
 
-https.createServer({
-  key: fs.readFileSync(argv['tls-key']),
-  cert: fs.readFileSync(argv['tls-cert'])
-}, app).listen(argv['listen-port']);
+https.createServer(proxyTLS, app).listen(argv['listen-port']);
 
