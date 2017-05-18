c89f18985281eb25ace840760588ae7a862f12eb
Jeff Cantrill <jcantril@redhat.com>
2017-05-18 09:55:55 -0400

    fix #14. num not found

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 set -eu
 
+if [ -n "${OAP_DEBUG:-}" ] ; then
+    set -x
+fi
+
 #set the max memory
 BYTES_PER_MEG=$((1024*1024))
-BYTES_PER_GIG=$((1024*${BYTES_PER_MEG}))
+BYTES_PER_GIG=$((1024*$BYTES_PER_MEG))
 
 DEFAULT_MIN=$((64 * $BYTES_PER_MEG)) #This is a guess
 regex='^([[:digit:]]+)([GgMm])?i?$'
@@ -14,13 +18,13 @@ unit_regex='[GgMm]'
 export NODE_OPTIONS=""
 
 if [ 1 -eq $(echo "${OCP_AUTH_PROXY_MEMORY_LIMIT:-}" | grep -Ec $regex) ] ; then
-    num=$(echo ${OCP_AUTH_PROXY_MEMORY_LIMIT} | grep -oE $num_regex)
-    unit=$(echo ${OCP_AUTH_PROXY_MEMORY_LIMIT} | grep -oE $unit_regex || echo "")
+    num=$(echo $OCP_AUTH_PROXY_MEMORY_LIMIT | grep -oE $num_regex)
+    unit=$(echo $OCP_AUTH_PROXY_MEMORY_LIMIT | grep -oE $unit_regex || echo "")
 
     if [ "$unit" = "G" ] || [ "$unit" = "g" ]; then
-        ((num = num * ${BYTES_PER_GIG})) # enables math to work out for odd Gi
+        num="$((num * $BYTES_PER_GIG))" # enables math to work out for odd Gi
     elif [ "$unit" = "M" ]  || [ "$unit" = "m" ]; then
-        ((num = num * ${BYTES_PER_MEG})) # enables math to work out for odd Gi
+        num="$((num * $BYTES_PER_MEG))" # enables math to work out for odd Gi
     #else assume bytes
     fi
 
@@ -29,7 +33,7 @@ if [ 1 -eq $(echo "${OCP_AUTH_PROXY_MEMORY_LIMIT:-}" | grep -Ec $regex) ] ; then
         num=$DEFAULT_MIN
     fi
 
-    export NODE_OPTIONS="--max-old-space-size=$((num / ${BYTES_PER_MEG}))"
+    export NODE_OPTIONS="--max-old-space-size=$((num / $BYTES_PER_MEG))"
 
 else
     echo "Unable to process the OCP_AUTH_PROXY_MEMORY_LIMIT: '${OCP_AUTH_PROXY_MEMORY_LIMIT}'."
